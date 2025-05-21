c0c32879204bd1a14a441cf954fd35dc5e3da3e6
davideerusso <rd.96.rd@gmail.com>
2025-05-21 17:11:49 +0200

    alert 2

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,7 +1,9 @@
 export default function Alert({ text, type }) {
   return (
-    <section className="container">
-      <div className={`alert alert-${type} `}>{text}</div>
+    <section className="container alert-space">
+      <div className={`alert alert-${type || "danger"} `}>
+        {`${text || "Si è verificato un errore"} `}
+      </div>
     </section>
   );
 }
--- src/Body.jsx
+++ src/Body.jsx
@@ -3,9 +3,8 @@ import Alert from "../src/Alert";
 export default function Main() {
   return (
     <main>
-      <Alert text="Errore Comics" type="danger" />
-
       <section className="back-color">
+        <Alert text="Good Comics" type="success" />
         <div className="container main-content">
           <h1>-- Content goes here--</h1>
         </div>
