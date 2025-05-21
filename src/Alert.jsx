6c67dc85d4c0a76a5799baae5f2a35cf8f73bb1d
davideerusso <rd.96.rd@gmail.com>
2025-05-21 17:21:51 +0200

    success

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,9 +1,14 @@
 export default function Alert({ text, type }) {
+  if (!type) {
+    return (
+      <section className="container alert-space">
+        <div className="alert alert-danger">Si è verificato un errore</div>
+      </section>
+    );
+  }
   return (
     <section className="container alert-space">
-      <div className={`alert alert-${type || "danger"} `}>
-        {`${text || "Si è verificato un errore"} `}
-      </div>
+      <div className={`alert alert-${type}`}>{text}</div>
     </section>
   );
 }
